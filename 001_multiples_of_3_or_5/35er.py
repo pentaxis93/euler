a158cbc70ebc4eddab591c72c8cb60446eee30d9
pentaxis93 <robert.david.grossman@gmail.com>
2022-05-29 17:02:49 +0200

    Cleaned up Problem 1.

--- 001_multiples_of_3_or_5/35er.py
+++ 001_multiples_of_3_or_5/35er.py
@@ -1,6 +1,13 @@
+'''Project Euler Problem 1: Multiples of 3 or 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.'''
+
+limit = int(input('Please set the top limit: '))
+
 n = 0
-for i in range(1, 1000):
+for i in range(1, limit):
     if (i % 3 == 0) | (i % 5 == 0):
-        print(i, 'qualifies')
-        n = n + i
+        n += i
 print(n)
