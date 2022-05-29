9d30cf8c61f43534c568796dee4a6823d4b97992
pentaxis93 <robert.david.grossman@gmail.com>
2022-05-29 23:07:38 +0200

    Updated pentamath module to latest version.

---
+++ modules/pentamath.py
@@ -0,0 +1,69 @@
+'''
+Tricky math functions.
+'''
+
+def isprime(number):
+    '''Test a number for primeness.'''
+
+    factors = []
+
+    for i in range(int(number ** 0.5) + 1):
+        if i < 4:
+            continue
+        if number % i == 0:
+            return False
+
+    return True
+
+def unique_factors(number):
+    '''Factor any positive integer.
+
+    Returns a list of unique prime factors.'''
+
+    factors = []
+    reduced = number
+
+    while True:
+        for i in range(2, number):
+            if number % i == 0:
+                if i not in factors:
+                    factors.append(i)
+                reduced = int(number / i)
+                break
+        if number == reduced:
+            break
+        else:
+            number = reduced
+
+    if number not in factors:
+        factors.append(number)
+    return factors
+
+def all_factors(number):
+    '''Factor any positive integer.
+
+    Returns a list of all prime factors, including duplicates.'''
+
+    factors = []
+    reduced = number
+
+    while True:
+        for i in range(2, number):
+            if number % i == 0:
+                factors.append(i)
+                reduced = int(number / i)
+                break
+        if number == reduced:
+            break
+        else:
+            number = reduced
+
+    factors.append(number)
+    return factors
+
+def ispalindrome(input):
+    '''Evaluate whether or not an input is palindromic.'''
+    if str(input)[:] == str(input)[::-1]:
+        return True
+    else:
+        return False
