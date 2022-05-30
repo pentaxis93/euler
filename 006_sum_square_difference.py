b62a9b75bbe6c2e5b556b3253fc5d99e069e522d
pentaxis93 <robert.david.grossman@gmail.com>
2022-05-30 11:11:16 +0200

    Reimplement Problem 6 with modular functions

--- 006_sum_square_difference.py
+++ 006_sum_square_difference.py
@@ -2,18 +2,13 @@
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.'''
 
-how_many=int(input('I\'ll find the difference between the sum of the squares and the square of the sums for the first ___ natural numbers. How many numbers would you like me to analyze? '))
+import pentamath
 
-sum_of_squares = 0
-sum = 0
-square_of_sum = 0
+n=int(input('I\'ll find the difference between the sum of the squares and the square of the sums for the first ___ natural numbers. How many numbers would you like me to analyze? '))
 
-for i in range(1, how_many + 1):
-    sum_of_squares += i ** 2
-    sum += i
+square_of_sum = pentamath.sum_of_n(n) ** 2
+sum_of_squares = pentamath.sum_of_n2(n)
 
-square_of_sum = sum ** 2
-
-print('The square of the sum is', square_of_sum, '.')
-print('The sum of the squares is', sum_of_squares, '.')
-print('The difference is', square_of_sum - sum_of_squares, '.')
+print('The square of the sum is: ', square_of_sum)
+print('The sum of the squares is: ', sum_of_squares)
+print('The difference is', square_of_sum - sum_of_squares)
