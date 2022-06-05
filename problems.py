c0643912796ced2789a81f1e01c4b3b29da000e9
pentaxis93 <robert.david.grossman@gmail.com>
2022-06-05 21:53:16 +0200

    Implement proper docstrings in problems module

--- problems.py
+++ problems.py
@@ -1,8 +1,12 @@
+"""This module provides functional implementations of a few Project
+Euler problems.
+"""
+
 import trickymath
 
 # Problem 1: Sum of Multiples
 def multipladd(x = 3, y = 5, z = 1000):
-    '''Return the sum of all natural numbers below z that are multiples of x or y.
-    '''
-
+    """Return the sum of all natural numbers below z that are multiples
+    of x or y.
+    """
     return trickymath.sum_of_multiples(x, z) + trickymath.sum_of_multiples(y, z) - trickymath.sum_of_multiples(x * y, z)
