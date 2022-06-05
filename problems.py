293224da269f5b5a5e4dd2372bc78760b9e8a385
pentaxis93 <robert.david.grossman@gmail.com>
2022-06-05 15:22:36 +0200

    Correct function call

--- problems.py
+++ problems.py
@@ -4,8 +4,14 @@ import trickymath
 
 os.system('clear')
 
+# Problem 1: Sum of Multiples
 def multipladd(x = 3, y = 5, z = 1000):
     '''Return the sum of all natural numbers below z that are multiples of x or y.
     '''
 
-    return sum_of_multiples(x, z) + sum_of_multiples(y, z) - sum_of_multiples(x * y, z)
+    return trickymath.sum_of_multiples(x, z) + trickymath.sum_of_multiples(y, z) - trickymath.sum_of_multiples(x * y, z)
+
+    end_clock_time = time.time()
+    end_cpu_time = time.process_time()
+
+    print('This calculation took {} seconds of clock time and {} seconds of CPU time.'.format(end_clock_time - start_clock_time, end_cpu_time - start_cpu_time))
