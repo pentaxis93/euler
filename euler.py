467b96d2603da2d5a959ada38c5ce2b363c8a542
pentaxis93 <robert.david.grossman@gmail.com>
2022-06-05 23:22:24 +0200

    Implement a basic wrapper for a problem

--- euler.py
+++ euler.py
@@ -1,3 +1,14 @@
+import time
 import problems
 
-print(problems.multipladd())
+# Problem 1
+start_clock_time = time.time()
+start_cpu_time = time.process_time()
+p1 = problems.multipladd()
+end_clock_time = time.time()
+end_cpu_time = time.process_time()
+clock_time = end_clock_time - start_clock_time
+cpu_time = end_cpu_time - start_cpu_time
+print('The solution to Problem 1 is {}.'.format(format(p1, ',')))
+print('''This calculation required {} seconds of clock time
+but only {} seconds of cpu time.'''.format(clock_time, cpu_time))
