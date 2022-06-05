0daf014417fb6cf697d5f0089052bb17fcd2a653
pentaxis93 <robert.david.grossman@gmail.com>
2022-06-05 15:24:34 +0200

    Remove more unnecessary code from functional implementation

--- problems.py
+++ problems.py
@@ -10,8 +10,3 @@ def multipladd(x = 3, y = 5, z = 1000):
     '''
 
     return trickymath.sum_of_multiples(x, z) + trickymath.sum_of_multiples(y, z) - trickymath.sum_of_multiples(x * y, z)
-
-    end_clock_time = time.time()
-    end_cpu_time = time.process_time()
-
-    print('This calculation took {} seconds of clock time and {} seconds of CPU time.'.format(end_clock_time - start_clock_time, end_cpu_time - start_cpu_time))
