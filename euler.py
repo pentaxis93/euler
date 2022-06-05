8ceafb23ca3ff519f4005ba77220fe511242144d
pentaxis93 <robert.david.grossman@gmail.com>
2022-06-05 23:57:59 +0200

    Implement Problem 2 the same way

--- euler.py
+++ euler.py
@@ -4,11 +4,25 @@ import problems
 # Problem 1
 start_clock_time = time.time()
 start_cpu_time = time.process_time()
-p1 = problems.multipladd()
+solution = problems.multipladd()
 end_clock_time = time.time()
 end_cpu_time = time.process_time()
 clock_time = end_clock_time - start_clock_time
 cpu_time = end_cpu_time - start_cpu_time
-print('The solution to Problem 1 is {}.'.format(format(p1, ',')))
+print('The solution to Problem 1 is {}.'.format(format(solution, ',')))
 print('''This calculation required {} seconds of clock time
-but only {} seconds of cpu time.'''.format(clock_time, cpu_time))
+but only {} seconds of cpu time.
+'''.format(clock_time, cpu_time))
+
+# Problem 2
+start_clock_time = time.time()
+start_cpu_time = time.process_time()
+solution = problems.evenfibs()
+end_clock_time = time.time()
+end_cpu_time = time.process_time()
+clock_time = end_clock_time - start_clock_time
+cpu_time = end_cpu_time - start_cpu_time
+print('The solution to Problem 2 is {}.'.format(format(solution, ',')))
+print('''This calculation required {} seconds of clock time
+but only {} seconds of cpu time.
+'''.format(clock_time, cpu_time))
--- problems.py
+++ problems.py
@@ -4,9 +4,22 @@ Euler problems.
 
 import trickymath
 
-# Problem 1: Sum of Multiples
+# Problem 1: Multiples of 3 or 5
 def multipladd(x = 3, y = 5, z = 1000):
     """Return the sum of all natural numbers below z that are multiples
     of x or y.
     """
     return trickymath.sum_of_multiples(x, z) + trickymath.sum_of_multiples(y, z) - trickymath.sum_of_multiples(x * y, z)
+
+# Problem 2: Even Fibonacci numbers
+def evenfibs(x = 4000000):
+    total = 0
+    a = 1
+    b = 1
+    c = a + b
+    while c <= x:
+        total += c
+        a = b + c
+        b = c + a
+        c = a + b
+    return total
