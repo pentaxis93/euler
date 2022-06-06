fce2b806168479f52878efc832f01cda96bbd819
pentaxis93 <robert.david.grossman@gmail.com>
2022-06-06 13:50:54 +0200

    Added tests for first two problems

---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+from problems import *
+
+class TestProblemSolvers(unittest.TestCase):
+    def test_multipladd(self):
+        self.assertEqual(multipladd(3, 5, 1000), 233168)
+
+    def test_evenfibs(self):
+        self.assertEqual(evenfibs(4000000), 4613732)
